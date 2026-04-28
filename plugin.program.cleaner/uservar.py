fd26efdfc23b0d72522346ad749bccd553e33585
Newf <ian@kellys.me>
2026-04-28 09:17:59 -0700

    ewfew

--- plugin.program.cleaner/uservar.py
+++ plugin.program.cleaner/uservar.py
@@ -1,11 +1,11 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://www.dropbox.com/scl/fi/vrdxxv76qvdce1kvklvpt/builds.xml?rlkey=894o2gw9lines2km55u8xvs5f&st=7529kfcf&dl=1'
+buildfile = 'https://www.dropbox.com/scl/fi/vrdxxv76qvdce1kvklvpt/builds.xml?rlkey=894o2gw9lines2km55u8xvs5f&dl=1'
 
 '''#####-----Videos File-----#####'''
-videos_url = 'https://github.com/newf276/junk/raw/master/wizard/Build%20Preview/MyKodiBuild.m4v'
+videos_url = 'https://www.dropbox.com/scl/fi/1d664o1q645rcww6z9fta/build-preview.m4v?rlkey=u95q6b7m1um6p3och0mehqk21&dl=1'
 
 '''#####-----Notification File-----#####'''
-notify_url  = 'https://github.com/newf276/junk/raw/master/wizard/notify.txt'
+notify_url  = 'https://www.dropbox.com/scl/fi/mu69l2myt99u79yd4cpwe/build-notify.txt?rlkey=b7kydu5o4wt0e2klg0fto4sqq&st=24bxjlpe&dl=1'
 
 '''#####-----Changelog Directory-----#####'''
 changelog_dir  = 'http://CHANGEME/'
